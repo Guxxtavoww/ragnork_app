3e97e8d14ba135aa2cbc3f132a50157376172014
Gustavo <gustavoaugustocar@hotmail.com>
2024-01-13 10:04:04 -0300

    Chore: test

--- src/server/lib/database.lib.ts
+++ src/server/lib/database.lib.ts
@@ -1,49 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-class PrimsaInstance {
-  private prismaInstance: Maybe<PrismaClient> = null;
-
-  constructor() {
-    this.prismaInstance = new PrismaClient();
-  }
-
-  getPrismaInstance() {
-    if (this.prismaInstance) {
-      this.prismaInstance.$connect();
-
-      return this.prismaInstance;
-    }
-
-    throw new Error('Prisma instance not initialized.');
-  }
-
-  async disconnect() {
-    if (this.prismaInstance) {
-      await this.prismaInstance.$disconnect();
-    }
-  }
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-const prismaInstance = new PrimsaInstance();
+export const db = globalThis.prisma || new PrismaClient();
 
-export async function performDatabaseOperation<T>(
-  callback: (prisma: PrismaClient) => Promise<T>
-): Promise<T> {
-  try {
-    const prisma = prismaInstance.getPrismaInstance();
-
-    const result = await callback(prisma);
-
-    return Promise.resolve(result);
-  } catch (error) {
-    prismaInstance.disconnect();
-
-    console.error(error);
-
-    throw error;
-  }
-}
-
-const prisma = prismaInstance.getPrismaInstance();
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
--- src/utils/auth.util.ts
+++ src/utils/auth.util.ts
@@ -3,10 +3,11 @@ import type { NextAuthOptions } from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import DiscordProvider from 'next-auth/providers/discord';
 
-import prisma from '@/server/lib/database.lib';
+import { db } from '@/server/lib/database.lib';
+
 
 export const authOptions = {
-  adapter: PrismaAdapter(prisma) as Adapter,
+  adapter: PrismaAdapter(db) as Adapter,
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_ID,
