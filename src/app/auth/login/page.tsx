229485d27fac53a117a32c5fee58f7c9298737f0
Gustavo <gustavoaugustocar@hotmail.com>
2024-01-13 10:31:26 -0300

    Chore: test

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,5 @@
-import { getAuthSession } from '@/server/lib/auth.lib';
 import { LoginForm } from './_components/login-form';
 
-export default async function Page() {
-  const session = await getAuthSession();
-
-  return session ? (
-    <pre>{JSON.stringify(session, null, 2)}</pre>
-  ) : (
-    <LoginForm />
-  );
+export default function Page() {
+  return <LoginForm />;
 }
